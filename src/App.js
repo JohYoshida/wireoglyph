58ac8d67ff2dd687f4f9ab6ea0ff9b3844e346b1
Joh Yoshida <johyoshida@gmail.com>
2021-05-05 23:56:47 -0700

    More descriptive default text, and some more fun words

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import './App.css';
 
+let mobyDick = "Call me Ishmael. Some years ago - never mind how long precisely - having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the watery part of the world. It is a way I have of driving off the spleen, and regulating circulation. Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodologically knocking people's hats off - then, I account it high time to get to sea as soon as I can. This is my substitute for pistol and ball... I quietly take to the ship. There is nothing surprising in this. If they but knew it, almost all men in their degree, some time or other, cherish very nearly the same feelings towards the ocean with me."
+
+let shapes = "test roar rear treat fluff bulb bomb blob neon aria nonagon terrible exposee"
+
+let funWords = "phenomenal lexicon scintillating communication bloviate excessively languages consideration cybernetic communism information steersmanship portobello deleuze phlebotomist technicality chromatic bellicose legible extensible honorific charismatic diabolical prehensile ascetic network flagellum gargantuan invalidation charismatic megafauna"
 
 function App() {
-  let mobyDick = "Call me Ishmael. Some years ago - never mind how long precisely - having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the watery part of the world. It is a way I have of driving off the spleen, and regulating circulation. Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodologically knocking people's hats off - then, I account it high time to get to sea as soon as I can. This is my substitute for pistol and ball... I quietly take to the ship. There is nothing surprising in this. If they but knew it, almost all men in their degree, some time or other, cherish very nearly the same feelings towards the ocean with me.";
-
-  let shapes = "test roar rear treat fluff bulb bomb blob aria nonagon terrible"
-  let longWords = "phenomenon deleuze communication cybernetic communism information lexicon phlebotomist technicality chromatic bellicose consideration languages bloviate legible extensible honorific charismatic diabolical prehensile"
   // State hooks
-  const [string, setString] = React.useState(mobyDick);
-  const [canvasSize, setC] = React.useState(3); // size multiplication factor for canvas
+  const [string, setString] = React.useState("This program converts text into the lines drawn between letters as they'd be inputted on a swipe keyboard. Type something here to try it out, or press one of the below buttons to see some more examples");
+  const [canvasSize, setC] = React.useState(1.5); // size multiplication factor for canvas
   // char positions on 100x50 canvas
   const chars = {
     1: [5, 5],
@@ -155,9 +156,10 @@ function App() {
         <div className="Buttons">
           <button name="Moby Dick" onClick={() => setString(mobyDick)}>Moby Dick</button>
           <button name="Geometry" onClick={() => setString(shapes)}>Geometry</button>
-          <button name="Long words" onClick={() => setString(longWords)}>Long words</button>
+          <button name="Long words" onClick={() => setString(funWords)}>Fun words</button>
         </div>
         <div>
+          <text>size</text>
           <input
             type="range"
             min="0.5"
@@ -168,7 +170,7 @@ function App() {
             id="sizeRange"
             onChange={changeCanvasSize}
           />
-        <text>{canvasSize}</text>
+        <text>{canvasSize}x</text>
         </div>
         <div className="Canvases">
           {Canvases}
